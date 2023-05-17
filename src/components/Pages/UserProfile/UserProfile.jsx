5cf626f6995bd548a43e8cb23eb1683ea7c3f6a5
Antu Saha <antusaha970@gmail.com>
2023-05-17 15:45:15 +0600

    added update user profile info functionality

--- src/components/Pages/UserProfile/UserProfile.jsx
+++ src/components/Pages/UserProfile/UserProfile.jsx
@@ -1,8 +1,18 @@
 import styled from "@emotion/styled";
-import { Box, Button, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  FormControl,
+} from "@mui/material";
 import { useState } from "react";
 import { useSelector } from "react-redux";
-
+import { useForm, Controller } from "react-hook-form";
 const ProfileBox = styled(Box)`
   width: 500px;
   min-height: 400px;
@@ -37,8 +47,17 @@ const TextItemBox = styled(Box)`
 const UserProfile = () => {
   const user = useSelector((state) => state.user.user);
   const [editState, setEditState] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const handleEdit = () => {
     setEditState(true);
+  };
+  const onSubmit = async (data) => {
+    setEditState(false);
+    console.log(data);
   };
   return (
     <Box
@@ -52,7 +71,135 @@ const UserProfile = () => {
             <ProfilePicture src={user.photoURL} alt="your profile picture" />
           </Box>
           {editState ? (
-            <p>edit</p>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Controller
+                name="name"
+                control={control}
+                defaultValue={user.name}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    id="nameInp"
+                    label="Your Name"
+                    variant="outlined"
+                    margin="normal"
+                    sx={{
+                      width: "320px",
+                    }}
+                  />
+                )}
+              />
+              <Controller
+                name="number"
+                rules={{ required: true, minLength: 11, maxLength: 11 }}
+                control={control}
+                defaultValue={user.number}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    id="numberInp"
+                    label="Your Number"
+                    variant="outlined"
+                    margin="normal"
+                    sx={{
+                      width: "320px",
+                    }}
+                  />
+                )}
+              />
+              {errors.number && (
+                <Typography
+                  component="p"
+                  variant="small"
+                  sx={{
+                    color: "red",
+                  }}
+                >
+                  Please Enter a valid number
+                </Typography>
+              )}
+              <Controller
+                name="bloodGroup"
+                control={control}
+                rules={{ required: true }}
+                defaultValue={user.bloodGroup}
+                render={({ field }) => (
+                  <FormControl fullWidth margin="normal">
+                    <InputLabel id="bloodGroupLabel">Blood Group</InputLabel>
+                    <Select
+                      {...field}
+                      labelId="bloodGroupLabel"
+                      id="bloodGroupInp"
+                      label="Blood Group"
+                    >
+                      <MenuItem value="O+">O+</MenuItem>
+                      <MenuItem value="O-">O-</MenuItem>
+                      <MenuItem value="A+">A+</MenuItem>
+                      <MenuItem value="A-">A-</MenuItem>
+                      <MenuItem value="B+">B+</MenuItem>
+                      <MenuItem value="B-">B-</MenuItem>
+                      <MenuItem value="AB+">AB+</MenuItem>
+                      <MenuItem value="AB-">AB-</MenuItem>
+                    </Select>
+                  </FormControl>
+                )}
+              />
+              {errors.bloodGroup && (
+                <Typography
+                  component="p"
+                  variant="small"
+                  sx={{
+                    color: "red",
+                  }}
+                >
+                  Please select your blood group
+                </Typography>
+              )}
+              <Controller
+                name="location"
+                control={control}
+                rules={{ required: true }}
+                defaultValue={user.location}
+                render={({ field }) => (
+                  <FormControl fullWidth margin="normal">
+                    <InputLabel id="locationLabel">Your Location</InputLabel>
+                    <Select
+                      {...field}
+                      labelId="locationLabel"
+                      id="locationInp"
+                      label="Your Location"
+                    >
+                      <MenuItem value="Feni">Feni</MenuItem>
+                      <MenuItem value="Dhaka">Dhaka</MenuItem>
+                      <MenuItem value="Chottogram">Chottogram</MenuItem>
+                    </Select>
+                  </FormControl>
+                )}
+              />
+              {errors.location && (
+                <Typography
+                  component="p"
+                  variant="small"
+                  sx={{
+                    color: "red",
+                  }}
+                >
+                  Please select your location
+                </Typography>
+              )}
+              <Button type="submit" color="secondary" variant="contained">
+                Save
+              </Button>
+            </Box>
           ) : (
             <>
               <TextItemBox>
