02c199e84042c2bce4e92df6a5a8fb333ebd4c60
Antu Saha <antusaha970@gmail.com>
2023-05-18 12:37:53 +0600

    removed static email changed

--- src/redux/slices/userSlice/userSlice.js
+++ src/redux/slices/userSlice/userSlice.js
@@ -1,36 +1,35 @@
-/* eslint-disable no-useless-catch */
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
   isLoggedIn: false,
   isReady: false,
-}
+};
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
-    addUserInfo: (state,action) => {
-        state.user = action.payload
-        state.isLoggedIn = true
-        state.user.email = "xyuz@gmail.com"
+    addUserInfo: (state, action) => {
+      state.user = action.payload;
+      state.isLoggedIn = true;
     },
-    addAdditionalInfo: (state,action) => {
-        const {number,location,bloodGroup,name} = action.payload;
-        state.user.number = number;
-        state.user.location = location;
-        state.user.bloodGroup = bloodGroup;
-        state.user.name = name;
-        state.isReady = true;
+    addAdditionalInfo: (state, action) => {
+      const { number, location, bloodGroup, name } = action.payload;
+      state.user.number = number;
+      state.user.location = location;
+      state.user.bloodGroup = bloodGroup;
+      state.user.name = name;
+      state.isReady = true;
     },
     removeUserInfo: (state) => {
-        state.user = null
-        state.isLoggedIn = false
+      state.user = null;
+      state.isLoggedIn = false;
     },
   },
-})
+});
 
-export const { addUserInfo, removeUserInfo,addAdditionalInfo } = userSlice.actions
+export const { addUserInfo, removeUserInfo, addAdditionalInfo } =
+  userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
