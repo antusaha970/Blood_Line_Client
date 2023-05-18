7793d2bf4d045191651dbe9535ebe8d4c5e65ad8
Antu Saha <antusaha970@gmail.com>
2023-05-18 13:27:26 +0600

    changed api url  for testing

--- src/API/API.js
+++ src/API/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:5000" 
-  });
+  baseURL: "https://blood-line-feni.onrender.com",
+});
 
-  export default client;
+export default client;
