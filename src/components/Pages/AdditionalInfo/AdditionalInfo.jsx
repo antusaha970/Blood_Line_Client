d2e2b74519ac949724e63a1aa805b953457800c2
Antu Saha <antusaha970@gmail.com>
2023-05-19 12:32:48 +0600

    added toast notification on additional info page

--- src/components/Pages/AdditionalInfo/AdditionalInfo.jsx
+++ src/components/Pages/AdditionalInfo/AdditionalInfo.jsx
@@ -16,6 +16,7 @@ import { addAdditionalInfo } from "../../../redux/slices/userSlice/userSlice";
 import { useNavigate } from "react-router";
 import client from "../../../API/API";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const AdditionalInfo = () => {
   const user = useSelector((state) => state.user.user);
@@ -78,6 +79,31 @@ const AdditionalInfo = () => {
     checkIfUserExist();
   }, [isReady]);
 
+  useEffect(() => {
+    toast(`Welcome ${user.name}`, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+    toast(
+      `If you already registered than please wait your data is being loaded else please submit the form with correct information`,
+      {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      }
+    );
+  }, []);
   return (
     <Box
       component="section"
